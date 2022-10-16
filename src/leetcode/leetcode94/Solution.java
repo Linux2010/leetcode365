5183b6b5f91498808e5849a6a82de96efa9f11ae
FANTA <35169750+JDZW2018@users.noreply.github.com>
2022-10-16 10:45:03 +0800

    //94. 二叉树的中序遍历

--- src/leetcode/leetcode94/Solution.java
+++ src/leetcode/leetcode94/Solution.java
@@ -7,6 +7,11 @@ import java.util.Stack;
 
 //https://leetcode.cn/problems/binary-tree-inorder-traversal/submissions/335492219/
 //94. 二叉树的中序遍历
+
+//首先我们需要了解什么是二叉树的中序遍历：按照访问左子树——根节点——右子树的方式遍历这棵树，
+// 而在访问左子树或者右子树的时候我们按照同样的方式遍历，直到遍历完整棵树。
+// 因此整个遍历过程天然具有递归的性质，我们可以直接用递归函数来模拟这一过程。
+
 public class Solution {
 
     //递归方式
