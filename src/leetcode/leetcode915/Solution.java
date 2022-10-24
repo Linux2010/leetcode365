8bb7a2b07f81b3a87f29c7d5a7e27467176e7dc4
FANTA <35169750+JDZW2018@users.noreply.github.com>
2022-10-24 22:52:55 +0800

    //915. 分割数组

--- src/leetcode/leetcode915/Solution.java
+++ src/leetcode/leetcode915/Solution.java
@@ -3,6 +3,10 @@ package leetcode.leetcode915;
 //https://leetcode.cn/problems/partition-array-into-disjoint-intervals/
 //915. 分割数组
 class Solution {
+    //1,记录左侧最大leftMax，与总体最大max
+    //2，判断左边最大是否大于当前遍历，大于说明符合加入左边，res=i，并且为了保障leftMax最大，需要把max赋值。
+    //3，否则max取，max = Math.max(nums[i],max)
+    //4，返回 res+1；
     public int partitionDisjoint(int[] nums) {
         int res = 0 ;
         int max = nums[res];
