431871468eb5b5d01a6f83fadb9da42a233b8d50
FANTA <35169750+JDZW2018@users.noreply.github.com>
2022-11-02 22:44:53 +0800

    //4. 寻找两个正序数组的中位数 //O(m+n) 解题方法

--- src/leetcode/leetcode4/Solution.java
+++ src/leetcode/leetcode4/Solution.java
@@ -3,63 +3,38 @@ package leetcode.leetcode4;
 //4. 寻找两个正序数组的中位数
 // https://leetcode.cn/problems/median-of-two-sorted-arrays/
 public class Solution {
+
+    //O(m+n) 解题方法
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int l1 = nums1.length;
-        int l2 = nums2.length;
-        int l1l2= l1+l2;
-        if((l2+l1)%2 == 0){
-            //偶数
-            double res1=0,res2=0;
-            int k1=(l2+l1)/2;
-            int k2=(l2+l1)/2 +1;
-            int i = 0;
-            while (i<=k2){
-                int n = 0,m=0;
-                if(n<l1&&m<l2){
-                    if(nums1[n]>=nums2[m]){
-                        res2=nums1[n];
-                        n++;
-                    }else {
-                        res2=nums2[m];
-                        m++;
-                    }
-                } else if (n>=l1) {
-                    res2=nums2[m];
-                    m++;
-                }else {
-                    res2=nums1[n];
-                    n++;
-                }
+        int len = nums1.length+nums2.length;
+        int[] tmp = new int[len];
+        int i = 0,j=0;
+        for (int k=0; k < len;k++) {
+            if(i>=nums1.length){
+                tmp[k] = nums2[j];
+                j++;
+            } else if (j >= nums2.length) {
+                tmp[k] = nums1[i];
+                i++;
+            } else if (nums1[i]>nums2[j]) {
+                tmp[k] = nums2[j];
+                j++;
+            }else {
+                tmp[k] = nums1[i];
                 i++;
             }
-            return (res1+res2)/2;
+        }
+        if(len%2 ==0){
+            return (tmp[len/2-1]+tmp[len/2])*0.5;
         }else {
-            //奇数
-            int res = 0;
-            int k = (l2+l1)/2 +1;
-            int i = 0;
-            while (i<=k){
-                int n = 0,m=0;
-                if(n<l1&&m<l2){
-                    if(nums1[n]>=nums2[m]){
-                        res=nums1[n];
-                        n++;
-                    }else {
-                        res=nums2[m];
-                        m++;
-                    }
-                } else if (n>=l1) {
-                    res=nums2[m];
-                    m++;
-                }else {
-                    res=nums1[n];
-                    n++;
-                }
-                i++;
-            }
-            return res;
+            return tmp[len/2+len%2-1];
         }
 
+    }
 
+    public static void main(String[] args) {
+        int[] n1 = new int[]{0,0,0,0,0};
+        int[] n2 = new int[]{-1,0,0,0,0,0,1};
+        new Solution().findMedianSortedArrays(n1,n2);
     }
 }
