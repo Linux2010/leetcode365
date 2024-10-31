2e84a828af6a6288bb0cb84218a94293fb98969a
FANTA <35169750+JDZW2018@users.noreply.github.com>
2024-10-31 11:27:13 +0800

    up

---
+++ src/leetcode/leetcode17/Solution.java
@@ -0,0 +1,37 @@
+package leetcode.leetcode17;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    final String[] MAPPING = new String[]{"","","abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    char[] arr;
+    char[] path;
+    List<String> ans = new ArrayList<>();
+
+    public List<String> letterCombinations(String digits) {
+        if(digits.isBlank()) return ans;
+        arr = digits.toCharArray();
+        path = new char[digits.length()];
+        dfs(0);
+        return ans;
+    }
+
+    private void dfs(int deep){
+        if(deep == arr.length){
+            ans.add(new String(path));
+            return;
+        }
+
+        int n = Integer.parseInt(arr[deep]+"");
+        for(char c: MAPPING[n].toCharArray()){
+            path[deep] = c;
+            dfs(deep+1);
+        }
+    }
+
+    public static void main(String[] args) {
+        List<String> strings = new Solution().letterCombinations("2344");
+        strings.forEach(System.out::println);
+    }
+}
---
+++ src/leetcode/leetcode3211/Solution.java
@@ -0,0 +1,9 @@
+package leetcode.leetcode3211;
+
+import java.util.List;
+
+class Solution {
+    public List<String> validStrings(int n) {
+        return null;
+    }
+}
