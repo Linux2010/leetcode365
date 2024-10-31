18013b0ea2993fa81f97cefca065967edf6f909a
FANTA <35169750+JDZW2018@users.noreply.github.com>
2024-10-31 16:54:12 +0800

    up 283

--- src/leetcode/leetcode283/Solution.java
+++ src/leetcode/leetcode283/Solution.java
@@ -4,7 +4,39 @@ package leetcode.leetcode283;
 //easy
 // https://leetcode.cn/problems/move-zeroes/
 public class Solution {
+    //快慢指针，快指针遍历，慢指针标记连续0的第一位置
     public void moveZeroes(int[] nums) {
-
+        int index = 0;
+        for (int i = 0; i < nums.length-1; i++) {
+            if(nums[i] == 0){
+                nums[index] = nums[i+1];
+                nums[i+1] = 0;
+            }
+            if(nums[index]!=0){
+                index++;
+            }
+        }
     }
+
+    //优化判断条件
+    public void moveZeroes_1(int[] nums) {
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] != 0){
+                int tmp = nums[index];
+                nums[index] = nums[i];
+                nums[i] = tmp;
+                index++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{0,0,0,1,2,3};
+        new Solution().moveZeroes_1(nums);
+        for (int num : nums) {
+            System.out.println(num);
+        }
+    }
+
 }
