43118cc36e7ed69182f652751cc5fa31fda1b2d4
FANTA <35169750+JDZW2018@users.noreply.github.com>
2024-10-31 15:30:10 +0800

    up leetcode49

--- src/leetcode/leetcode49/Solution.py
+++ src/leetcode/leetcode49/Solution.py
@@ -1,1 +1,18 @@
-print(123)
+from collections import defaultdict
+from typing import List
+
+
+"""
+学习使用defaultdict，sorted，d.append list()
+"""
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        d = defaultdict(list)
+        for s in strs:
+            d[''.join(sorted(s))].append(s)
+        return list(d.values())
+
+if __name__ == '__main__':
+    strs = ["eat","tea","tan","ate","nat","bat"]
+    list = Solution().groupAnagrams(strs)
+    print(list)
