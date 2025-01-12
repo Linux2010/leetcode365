2df32d07a4810ab373335dd2617bd12be71a5e4a
FANTA <35169750+JDZW2018@users.noreply.github.com>
2025-01-12 14:28:40 +0800

    up

--- src/leetcode/leetcode2275/Solution.java
+++ src/leetcode/leetcode2275/Solution.java
@@ -1,5 +1,16 @@
 package leetcode.leetcode2275;
 
+import java.util.Arrays;
+
 public class Solution {
-    
+    public int largestCombination(int[] candidates) {
+        int[] ans = new int[24];
+        for(int v : candidates){
+            for(int i=0;v>0;i++){
+                ans[i] += v&1;
+                v >>= 1;
+            }
+        }
+        return Arrays.stream(ans).max().getAsInt();
+    }
 }
--- src/leetcode/leetcode2275/SolutionScala.scala
+++ src/leetcode/leetcode2275/SolutionScala.scala
@@ -1,5 +1,24 @@
 package leetcode.leetcode2275
 
 object SolutionScala {
+  def largestCombination(candidates: Array[Int]): Int = {
+    val ans = new Array[Int](24)
+    for (v <- candidates){
+      var vv = v
+      var i =0
+      while (vv>0){
+        ans(i) += vv&1
+        vv >>= 1
+        i += 1
+      }
+    }
+    ans.max
+  }
+
+  def main(args: Array[String]): Unit = {
+    val input = Array(16, 17, 71, 62, 12, 24, 14)
+    val ans = largestCombination(input)
+    println(ans)
+  }
 
 }
