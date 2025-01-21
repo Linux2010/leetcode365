32eb8092217268843cd2b4e56c728d098a4a3772
FANTA <35169750+JDZW2018@users.noreply.github.com>
2025-01-21 14:18:56 +0800

    up

--- src/leetcode/leetcode70/Solution.java
+++ src/leetcode/leetcode70/Solution.java
@@ -35,6 +35,15 @@ public class Solution {
     }
 
 
+    public int climbStairs_dp (int n){
+        int[] ans = new int[n+1];
+        ans[0] = 1;
+        ans[1] = 1;
+        for (int i = 2; i < n+1; i++) {
+            ans[i] = ans[i-1] + ans[i-2];
+        }
+        return ans[n+1];
+    }
 
 
 }
