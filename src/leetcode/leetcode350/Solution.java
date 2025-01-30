4cd6fdfce6e57c9c4f8014a5a751b9c29b2d22d0
FANTA <35169750+JDZW2018@users.noreply.github.com>
2025-01-30 10:41:43 +0800

    up：350优化命名

--- src/leetcode/leetcode350/Solution.java
+++ src/leetcode/leetcode350/Solution.java
@@ -4,20 +4,28 @@ import java.util.*;
 
 public class Solution {
 
+    //https://leetcode.cn/problems/intersection-of-two-arrays-ii/?envType=daily-question&envId=2025-01-30
+
+    /**
+     * 1，优化命名，常识规范化
+     * 2，思路最优化，从暴力算法逐步切换到最短路径求解
+     * @param nums1
+     * @param nums2
+     * @return
+     */
     public int[] intersect(int[] nums1, int[] nums2) {
         ArrayList<Integer> res = new ArrayList<>();
-        int[] ints = new int[1001];
-        for (int i : nums1) {
-            ints[i]++;
+        int[] cnt = new int[1001];
+        for (int x : nums1) {
+            cnt[x]++;
         }
-        for (int j : nums2) {
-            if(ints[j] > 0){
-                res.add(j);
-                ints[j]--;
+        for (int x : nums2) {
+            if(cnt[x] > 0){
+                res.add(x);
+                cnt[x]--;
             }
 
         }
-
         return res.stream().mapToInt(Integer::intValue).toArray();
     }
 }
